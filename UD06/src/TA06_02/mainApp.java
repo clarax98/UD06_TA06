184e1458e6411e798fbedcb7bee6a27997572ebf
Clara Ogalla <claraogalla@gmail.com>
2022-07-10 18:26:27 +0200

    TA06_02

---
+++ UD06/src/TA06_02/mainApp.java
@@ -0,0 +1,30 @@
+package TA06_02;
+
+import java.util.Iterator;
+
+import javax.swing.JOptionPane;
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String cNumeros , cMax , cMin ;
+		do {
+			cNumeros = JOptionPane.showInputDialog("Cantidad de numeros a generar");
+		} while (cNumeros.isEmpty());
+		do {
+			cMax = JOptionPane.showInputDialog("Numero Maximo");
+		} while (cMax.isEmpty());
+		do {
+			cMin = JOptionPane.showInputDialog("Numero minimo");
+		} while (cMin.isEmpty());
+		
+		for (int i = 1; i <= Integer.parseInt(cNumeros); i++) {
+			JOptionPane.showMessageDialog(null, genNumeros(Integer.parseInt(cMax), Integer.parseInt(cMin)));
+		}
+		
+	}
+	public static int genNumeros(int cMax, int CMin) {
+		return (int) (Math.random() * ( cMax - CMin ));	
+	}
+}
